7072fd7fb905b60f53a1ce538cadd13a1fe93d60
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-09-19 17:33:31 +0200

    Add files via upload

---
+++ Sikidomok/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Síkidomok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sikidom> sikidomok = new List<Sikidom>();
+            sikidomok.Add(new Kor());
+            sikidomok.Add(new Teglalap());
+            sikidomok.Add(new Negyzet());
+        }
+    }
+}
